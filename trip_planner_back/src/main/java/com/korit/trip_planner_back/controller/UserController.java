079ac22ff6a9187e8337ffb3707b81c02dba1a83
HyeokJung803 <jh010803@naver.com>
2026-01-19 17:24:22 +0900

    ãã유ì저매퍼xml 프로바이더,role, 추가

---
+++ trip_planner_back/src/main/java/com/korit/trip_planner_back/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.korit.trip_planner_back.controller;
+
+import com.korit.trip_planner_back.security.PrincipalUser;
+import com.korit.trip_planner_back.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getMe(@AuthenticationPrincipal PrincipalUser principalUser) {
+        return ResponseEntity.ok(principalUser.getUser());
+    }
+
+    // 닉넴 업데이트
+    @PatchMapping("/nickname")
+    public ResponseEntity<?> updateNickname(
+            @AuthenticationPrincipal PrincipalUser principalUser,
+            @RequestBody Map<String, String> request) {
+        String newNickname = request.get("nickname");
+        int userId = principalUser.getUser().getUserId();
+
+        userService.updateNickname(userId, newNickname);
+        return ResponseEntity.ok("닉네임 업데이트 성공");
+    }
+}

1e2408503c3061386b1f46c6a13f99a2df0a4ed4
HyeokJung803 <jh010803@naver.com>
2026-01-19 16:13:59 +0900

    jwt토큰프로ë바이더 생성

--- trip_planner_back/src/main/java/com/korit/trip_planner_back/security/PrincipalUser.java
+++ trip_planner_back/src/main/java/com/korit/trip_planner_back/security/PrincipalUser.java
@@ -1,4 +1,27 @@
 package com.korit.trip_planner_back.security;
 
-public class PrincipalUser {
+import com.korit.trip_planner_back.entity.User;
+import lombok.Getter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+public class PrincipalUser extends DefaultOAuth2User {
+    private final User user;
+
+    public PrincipalUser(Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes, String nameAttributeKey, User user) {
+        super(authorities, attributes, nameAttributeKey);
+        this.user = user;
+    }
+
+    public static PrincipalUser getAuthenticatedPrincipalUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PrincipalUser principalUser = (PrincipalUser) authentication.getPrincipal();
+        return principalUser;
+    }
 }
---
+++ trip_planner_back/src/main/java/com/korit/trip_planner_back/service/UserService.java
@@ -0,0 +1,9 @@
+package com.korit.trip_planner_back.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+}
